8a7429e4be1f5bfd4c2e3ae7168d8ebc17ccb3c2
pellis_simone <simone.pellis@euris.it>
2023-07-06 12:28:39 +0200

    Now the month_rapportini_creation script creates an actual table.

--- script_excel/month_rapportini_creation.py
+++ script_excel/month_rapportini_creation.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import calendar
 import os
 from openpyxl.utils import get_column_letter
+from openpyxl.worksheet.table import Table, TableStyleInfo
 
 # Creazione del file Excel
 workbook = openpyxl.Workbook()
@@ -15,15 +16,35 @@ month = now.month
 num_days = calendar.monthrange(year, month)[1]
 month_name = now.strftime("%B")
 
-# Aggiunta dei giorni come colonne
-for day in range(1, num_days + 1):
-    column_letter = get_column_letter(day + 2)  # day + 1 per evitare di sovrascrivere la colonna A e B
-    cell = f"{column_letter}1"
-    sheet[cell] = f"{day} {month_name}"
-
 # Aggiunta delle righe argomento
 sheet["A2"] = "argomento 1"
 sheet["A3"] = "argomento 2"
+
+# Aggiunta dei giorni come colonne
+for day in range(1, num_days + 1):
+    column_letter = get_column_letter(day + 1)  # day + 1 per evitare di sovrascrivere la colonna A
+    cell = f"{column_letter}1"
+    sheet[cell] = f"{day} {month_name}"
+
+# Creazione della tabella
+table_range = f"A1:{get_column_letter(num_days + 1)}2"
+table = Table(displayName="Table1", ref=table_range)
+table.tableStyleInfo = TableStyleInfo(name="TableStyleMedium9", showFirstColumn=False, showLastColumn=False,
+                                      showRowStripes=True, showColumnStripes=False)
+sheet.add_table(table)
+
+# Imposta larghezza delle colonne
+for col in sheet.columns:
+    max_length = 0
+    column = col[0].column_letter
+    for cell in col:
+        try:
+            if len(str(cell.value)) > max_length:
+                max_length = len(cell.value)
+        except:
+            pass
+    adjusted_width = (max_length + 2) * 1.2
+    sheet.column_dimensions[column].width = adjusted_width
 
 # Creazione della directory se non esiste
 directory = "file_rapportini"
